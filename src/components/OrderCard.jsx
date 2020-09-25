6e7f8759f1be69cf708754d2951263382648144c
roxanaguerra <sguerraapaza@gmail.com>
2020-09-24 19:14:15 -0500

    modificando el card de los pedidos

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -3,22 +3,25 @@ import React from 'react';
 const OrderCard = ({order}) => {
   return (
     <div className="status">
-      <p className="">
+      <p>
         <span>Nº Pedido: </span>
-        <span className="info">1</span>
+        <span className="info">5</span>
       </p>
-      <p className="">
-        <span>Cliente: </span>
+      <p>
+        <span className="name-info">Cliente: </span>
         <span className="info">{order.client} </span>
-        <span>N° de Mesa: </span>
-        <span className="info">{order.table}</span>
+        <span className="name-info">N° de Mesa: </span>
+        <span className="info">{order.table} </span>        
       </p>
-      <p className="">
-        <span>Fecha de Pedido: </span>
-        <span className="info">{new Date(order.date).toLocaleString('es-PE')}</span>
+      <p>          
         <span>Tiempo Transcurrido: </span>
-        <span className="info">03 min</span>
+        <span className="info">03 min </span>
       </p>
+      <p>
+        <span>Fecha de Pedido: </span>
+        <span className="info">{new Date(order.date).toLocaleString('es-PE')} </span>
+      </p>    
+
       <table className="tableOrderCard">
         <thead>
           <tr>
@@ -30,9 +33,9 @@ const OrderCard = ({order}) => {
         <tbody>
           {order.products.map((item) => (
             <tr key={item.id}>
-              <td>{item.id}</td>
+              <td>{item.quantity}</td>
               <td>{item.productName}</td>
-              <td>{item.price}</td>
+              <td>{item.price.toFixed(2)}</td>
             </tr>
           ))}
         </tbody>
@@ -45,4 +48,5 @@ const OrderCard = ({order}) => {
       </button>
     </div>
   );
+};
 export default OrderCard;
